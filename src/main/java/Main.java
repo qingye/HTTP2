dde07d7b982de3dc502c8dc1fa556a368e14369c
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2018-04-23 12:48:00 +0200

    asdfghj

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,28 +2,44 @@ import connections.Connection;
 import frames.DataFrame;
 import streams.Stream;
 
+import javax.net.ssl.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, KeyManagementException, CertificateException, KeyStoreException, UnrecoverableKeyException {
+
+        SSLContext context = SSLContext.getInstance("TLSv1.2");
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("PKIX");
+        char[] password = "123456".toCharArray();
+        KeyStore ks = KeyStore.getInstance(new File("C:\\Program Files\\Java\\jdk-10.0.1\\bin\\keystore"), password);
+        kmf.init(ks, password);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX");
+        tmf.init(ks);
+
+        context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), SecureRandom.getInstance("SHA1PRNG"));
+        SSLServerSocketFactory ssf = context.getServerSocketFactory();
 
 //        ServerSocketFactory ssf = SSLServerSocketFactory.getDefault();
+
+        ServerSocket ss = ssf.createServerSocket(80);
+
+        SSLSocket s = (SSLSocket) ss.accept();
+        s.startHandshake();
+
+
+//        ServerSocket ss = new ServerSocket(80);
 //
-//        ServerSocket ss = ssf.createServerSocket(80);
-//
-//        SSLSocket s = (SSLSocket) ss.accept();
-//        s.startHandshake();
-
-
-        ServerSocket ss = new ServerSocket(80);
-
-        Socket s = ss.accept();
+//        Socket s = ss.accept();
 
         System.out.println("Connected...");
 
