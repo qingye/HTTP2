378748896bccf095e5265f0891b3bebcb6e02aee
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2018-04-26 15:43:17 +0200

    Javadoc.

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,22 @@
-typeSearchIndex = [{"p":"connections","l":"AbstractConnection"},{"p":"frames","l":"Compressor"},{"p":"example","l":"Connection"},{"p":"connections","l":"ConnectionInterface"},{"p":"connections","l":"ConnectionSettings"},{"p":"connections","l":"ConnectionThread"},{"p":"frames","l":"ContinuationFrame"},{"p":"frames","l":"DataFrame"},{"p":"example","l":"EchoServer"},{"p":"frames","l":"ErrorCode"},{"p":"frames","l":"Flags"},{"p":"frames","l":"Frame"},{"p":"frames","l":"FrameType"},{"p":"frames","l":"GoAwayFrame"},{"p":"frames","l":"HeadersFrame"},{"p":"example","l":"Main"},{"p":"frames","l":"PingFrame"},{"p":"frames","l":"PriorityFrame"},{"p":"example","l":"ProcessRequest"},{"p":"frames","l":"PushPromiseFrame"},{"p":"frames","l":"RSTStreamFrame"},{"p":"frames","l":"Setting"},{"p":"frames","l":"SettingsFrame"},{"p":"streams","l":"Stream"},{"p":"streams","l":"StreamState"},{"p":"frames","l":"WindowUpdateFrame"}]
+typeSearchIndex = [{"p": "connections", "l": "AbstractConnection"}, {"p": "frames", "l": "Compressor"}, {
+    "p": "example",
+    "l": "Connection"
+}, {"p": "connections", "l": "ConnectionInterface"}, {"p": "connections", "l": "ConnectionSettings"}, {
+    "p": "frames",
+    "l": "ContinuationFrame"
+}, {"p": "frames", "l": "DataFrame"}, {"p": "example", "l": "EchoServer"}, {
+    "p": "frames",
+    "l": "ErrorCode"
+}, {"p": "frames", "l": "Flags"}, {"p": "frames", "l": "Frame"}, {"p": "frames", "l": "FrameType"}, {
+    "p": "frames",
+    "l": "GoAwayFrame"
+}, {"p": "frames", "l": "HeadersFrame"}, {"p": "example", "l": "Main"}, {
+    "p": "frames",
+    "l": "PingFrame"
+}, {"p": "frames", "l": "PriorityFrame"}, {"p": "frames", "l": "PushPromiseFrame"}, {
+    "p": "frames",
+    "l": "RSTStreamFrame"
+}, {"p": "frames", "l": "Setting"}, {"p": "frames", "l": "SettingsFrame"}, {
+    "p": "streams",
+    "l": "Stream"
+}, {"p": "streams", "l": "StreamState"}, {"p": "frames", "l": "WindowUpdateFrame"}];
